783cd3fe1da15acfcb77794871e7dba879192d7c
Tom Breuel <tmbdev@gmail.com>
2015-04-28 10:59:10 -0700

    more documentation (notebooks)

--- version.h
+++ version.h
@@ -1,10 +1,15 @@
 
 inline const char *hg_version() {
-    return "a62cfd5fc054";
+    return "b10887adbec9+";
 }
 inline const char *hg_log() {
-    return R"(changeset:   90:a62cfd5fc054
+    return R"(changeset:   91:b10887adbec9
 tag:         tip
+user:        Tom <tmb@google.com>
+date:        Thu Apr 23 14:33:08 2015 -0700
+summary:     code reformatting
+
+changeset:   90:a62cfd5fc054
 user:        Tom <tmb@google.com>
 date:        Thu Apr 23 14:31:12 2015 -0700
 summary:     code reformatting
@@ -13,11 +18,6 @@ changeset:   89:ca2fac9a9a5c
 user:        Tom <tmb@google.com>
 date:        Thu Apr 23 14:28:43 2015 -0700
 summary:     text training
-
-changeset:   88:c45a6ffebac6
-user:        Tom <tmb@google.com>
-date:        Thu Apr 23 14:13:03 2015 -0700
-summary:     added text training examples
 
 )";
 }
