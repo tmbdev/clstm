4653967a3e366a5ee271e4ec473e2c000112fdcd
Tom Breuel <tmbdev@gmail.com>
2015-10-16 13:56:48 -0700

    Eliminated the Eigen::Matrix related macros.

--- clstm_compute.cc
+++ clstm_compute.cc
@@ -1,60 +1,158 @@
 #include "clstm_compute.h"
 
-#define A array()
-
 namespace ocropus {
 
-#define DOT(M, V) ((M) * (V))
-#define MATMUL(A, B) ((A) * (B))
-#define MATMUL_TR(A, B) ((A).transpose() * (B))
-#define MATMUL_RT(A, B) ((A) * (B).transpose())
-#define EMUL(U, V) ((U).array() * (V).array()).matrix()
-#define EMULV(U, V) ((U).array() * (V).array()).matrix()
-#define TRANPOSE(U) ((U).transpose())
-#define COL(A, b) (A).col(b)
-#define MAPFUNC(M, F) ((M).unaryExpr(F))
-#define SUMREDUCE(M) float(M.sum())
-#define BLOCK(A, i, j, n, m) (A).block(i, j, n, m)
-#define CBUTFIRST(M) BLOCK((M), 0, 1, (M).rows(), (M).cols() - 1)
-#define CFIRST(M) COL(M, 0)
-#define HOMDOT(A1, B) (DOT(CBUTFIRST(A1), B).colwise() + CFIRST(A1))
+#define CBUTFIRST(M) (M).block(0,1,(M).rows(),(M).cols()-1)
+#define CFIRST(M) (M).col(0)
 
-inline void ADDCOLS(Mat &m, Vec &v) {
-  for (int i = 0; i < COLS(m); i++)
-    for (int j = 0; j < ROWS(m); j++) m(j, i) += v(j);
+typedef vector<int> Classes;
+typedef vector<Classes> BatchClasses;
+
+// random initialization of sequences etc.
+
+void randgauss(Mat &m) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<double> randn;
+  for (int i = 0; i < ROWS(m); i++)
+    for (int j = 0; j < COLS(m); j++) m(i, j) = randn(gen);
 }
+
+void randgauss(Vec &v) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<double> randn;
+  for (int i = 0; i < ROWS(v); i++) v(i) = randn(gen);
+}
+
+void randinit(Mat &m, float s, const string mode) {
+  if (mode == "unif") {
+    m.setRandom();
+    m = (2 * s * m).array() - s;
+  } else if (mode == "pos") {
+    m.setRandom();
+    m = m * s;
+  } else if (mode == "normal") {
+    randgauss(m);
+    m = m * s;
+  }
+}
+
+void randinit(Vec &m, float s, const string mode) {
+  if (mode == "unif") {
+    m.setRandom();
+    m = (2 * s * m).array() - s;
+  } else if (mode == "pos") {
+    m.setRandom();
+    m = m * s;
+  } else if (mode == "normal") {
+    randgauss(m);
+    m = m * s;
+  }
+}
+void randinit(Batch &m, int no, int ni, float s, const string mode) {
+  m.resize(no, ni);
+  randinit(m.v, s, mode);
+}
+void randinit(Mat &m, int no, int ni, float s, const string mode) {
+  m.resize(no, ni);
+  randinit(m, s, mode);
+}
+void randinit(Vec &m, int no, float s, const string mode) {
+  m.resize(no);
+  randinit(m, s, mode);
+}
+void zeroinit(Mat &m, int no, int ni) {
+  m.resize(no, ni);
+  m.setZero();
+}
+void zeroinit(Vec &m, int no) {
+  m.resize(no);
+  m.setZero();
+}
+
+void resize(Sequence &seq, int nsteps, int dims, int bs) {
+  seq.resize(nsteps);
+  for (int i = 0; i < nsteps; i++) seq[i].resize(dims, bs);
+}
+
+int size(Sequence &seq, int dim) {
+  if (dim == 0) return seq.size();
+  if (dim == 1) return seq[0].rows();
+  if (dim == 2) return seq[0].cols();
+  THROW("bad dim ins size");
+  return -1;
+}
+
+Vec timeslice(const Sequence &s, int i, int b) {
+  Vec result(s.size());
+  for (int t = 0; t < s.size(); t++) result[t] = s[t].v(i, b);
+  return result;
+}
+
+// checking for NaNs in different objects
+
+bool anynan(Mat &a) {
+  for (int j = 0; j < ROWS(a); j++) {
+    for (int k = 0; k < COLS(a); k++) {
+      float x = a(j, k);
+      if (isnan(x)) return true;
+    }
+  }
+}
+
+bool anynan(Batch &a) {
+  return anynan(a.v) || anynan(a.d);
+}
+
+bool anynan(Sequence &a) {
+  for (int i = 0; i < a.size(); i++)
+    if (anynan(a[i])) return true;
+  return false;
+}
+
+// clipping
 
 void gradient_clip(Sequence &s, Float m) {
   if (m < 0) return;
   for (int t = 0; t < s.size(); t++) {
     s[t].d =
-        MAPFUNC(s[t].d, [m](Float x) { return x > m ? m : x < -m ? -m : x; });
+      s[t].d.unaryExpr([m](Float x) { return x > m ? m : x < -m ? -m : x; });
   }
 }
 
 void gradient_clip(Mat &d, Float m) {
   if (m < 0) return;
-  d = MAPFUNC(d, [m](Float x) { return x > m ? m : x < -m ? -m : x; });
+  d = 
+    d.unaryExpr([m](Float x) { return x > m ? m : x < -m ? -m : x; });
 }
 
 void gradient_clip(Batch &b, Float m) { gradient_clip(b.d, m); }
+
+// helper functions for Eigen::Tensor axes and sizes
 
 inline array<Eigen::IndexPair<int>,1> axes(int i,int j) {
   array<Eigen::IndexPair<int>,1> result = {Eigen::IndexPair<int>(i,j)};
   return result;
 }
+
 inline array<ptrdiff_t,1> ar(int i) {
   return array<ptrdiff_t,1>({i});
 }
+
 inline array<ptrdiff_t,2> ar(int i,int j) {
   return array<ptrdiff_t,2>({i,j});
 }
+
 inline Eigen::Sizes<1> S(int i) {
   return Eigen::Sizes<1>({i});
 }
+
 inline Eigen::Sizes<2> S(int i,int j) {
   return Eigen::Sizes<2>({i,j});
 }
+
+// forward and backward steps
 
 #ifndef USEMAT
 template <class F>
@@ -86,12 +184,12 @@ void backward_full1(Batch &y, Params &W1, Batch &x, Float gc) {
   Mat temp;
   temp = y.d;
   F::df(temp, y.v);
-  x.d = MATMUL_TR(CBUTFIRST(W1.v), temp);
+  x.d = CBUTFIRST(W1.v).transpose() * temp;
   int bs = y.v.cols();
   auto d_W = CBUTFIRST(W1.d);
-  d_W += MATMUL_RT(temp, x.v);
+  d_W += temp * x.v.transpose();
   auto d_w = CFIRST(W1.d);
-  for (int b = 0; b < bs; b++) d_w += COL(temp, b);
+  for (int b = 0; b < bs; b++) d_w += temp.col(b);
 }
 #endif
 template void forward_full1<NoNonlin>(Batch &y, Params &W, Batch &x);
@@ -106,7 +204,6 @@ template void backward_full1<TanhNonlin>(Batch &y, Params &W, Batch &x,
 template void backward_full1<ReluNonlin>(Batch &y, Params &W, Batch &x,
                                          Float gc);
 
-// compute non-linear full layers
 #ifndef USEMAT
 template <class F>
 void forward_full(Batch &y, Params &W, Batch &x) {
@@ -123,14 +220,14 @@ void backward_full(Batch &y, Params &W, Batch &x, Float gc) {
 #else
 template <class F>
 void forward_full(Batch &y, Params &W, Batch &x) {
-  y.v = nonlin<F>(MATMUL(W.v, x.v));
+  y.v = nonlin<F>(W.v * x.v);
 }
 template <class F>
 void backward_full(Batch &y, Params &W, Batch &x, Float gc) {
-  Mat temp = EMUL(yprime<F>(y.v), y.d);
+  Mat temp = yprime<F>(y.v).array() *  y.d.array();
   gradient_clip(temp, gc);
-  x.d += MATMUL_TR(W.v, temp);
-  W.d += MATMUL_RT(temp, x.v);
+  x.d += W.v.transpose() * temp;
+  W.d += temp * x.v.transpose();
 }
 #endif
 template void forward_full<NoNonlin>(Batch &y, Params &W, Batch &x);
@@ -173,7 +270,8 @@ void forward_softmax(Batch &z, Params &W1, Batch &x) {
   int n = ROWS(W1.v);
   int m = COLS(W1.v);
   int bs = COLS(x.v);
-  z.v = MAPFUN(HOMDOT(W1.v, x.v), limexp);
+  Float (*f)(Float) = limexp;
+  z.v = ((CBUTFIRST(W1.v) * x.v).colwise() + CFIRST(W1.v)).unaryExpr(f);
   for (int b = 0; b < bs; b++) {
     double total = 0.0;
     for(int i=0; i<n; i++) total += z.v(i,b);
@@ -181,9 +279,9 @@ void forward_softmax(Batch &z, Params &W1, Batch &x) {
   }
 }
 void backward_softmax(Batch &z, Params &W1, Batch &x) {
-  x.d = MATMUL_TR(CBUTFIRST(W1.v), z.d);
+  x.d = CBUTFIRST(W1.v).transpose() * z.d;
   auto d_W = CBUTFIRST(W1.d);
-  d_W += MATMUL_RT(z.d, x.v);
+  d_W += z.d * x.v.transpose();
   int n = ROWS(W1.v);
   int bs = COLS(z.v);
   Vec d_w= CFIRST(W1.d);
@@ -200,16 +298,16 @@ void forward_stack(Batch &z, Batch &x, Batch &y) {
   int ny = y.rows();
   int bs = x.cols();
   //z.resize(nx + ny, bs);
-  BLOCK(z.v, 0, 0, nx, bs) = x.v;
-  BLOCK(z.v, nx, 0, ny, bs) = y.v;
+  z.v.block(0,0,nx,bs) = x.v;
+  z.v.block(nx,0,ny,bs) = y.v;
 }
 void backward_stack(Batch &z, Batch &x, Batch &y) {
   assert(x.cols() == y.cols());
   int nx = x.rows();
   int ny = y.rows();
   int bs = x.cols();
-  x.d += BLOCK(z.d, 0, 0, nx, bs);
-  y.d += BLOCK(z.d, nx, 0, ny, bs);
+  x.d += z.d.block( 0, 0, nx, bs);
+  y.d += z.d.block( nx, 0, ny, bs);
 }
 
 void forward_stack(Batch &z, Batch &x, Sequence &y, int last) {
@@ -218,19 +316,19 @@ void forward_stack(Batch &z, Batch &x, Sequence &y, int last) {
   int ny = y.rows();
   int bs = x.cols();
   //z.resize(nx + ny, bs);
-  BLOCK(z.v, 0, 0, nx, bs) = x.v;
+  z.v.block( 0, 0, nx, bs) = x.v;
   if (last >= 0)
-    BLOCK(z.v, nx, 0, ny, bs) = y[last].v;
+    z.v.block( nx, 0, ny, bs) = y[last].v;
   else
-    BLOCK(z.v, nx, 0, ny, bs).setZero();
+    z.v.block( nx, 0, ny, bs).setZero();
 }
 void backward_stack(Batch &z, Batch &x, Sequence &y, int last) {
   assert(x.cols() == y.cols());
   int nx = x.rows();
   int ny = y.rows();
   int bs = x.cols();
-  x.d += BLOCK(z.d, 0, 0, nx, bs);
-  if (last >= 0) y[last].d += BLOCK(z.d, nx, 0, ny, bs);
+  x.d += z.d.block( 0, 0, nx, bs);
+  if (last >= 0) y[last].d += z.d.block( nx, 0, ny, bs);
 }
 void forward_reverse(Sequence &y, Sequence &x) {
   int N = x.size();
@@ -243,6 +341,7 @@ void backward_reverse(Sequence &y, Sequence &x) {
 }
 
 // stack the delayed output on the input
+
 void forward_stack1(Batch &all, Batch &inp, Sequence &out, int last) {
   assert(inp.cols() == out.cols());
   int bs = inp.cols();
@@ -250,12 +349,12 @@ void forward_stack1(Batch &all, Batch &inp, Sequence &out, int last) {
   int no = out.rows();
   int nf = ni + no + 1;
   //all.resize(nf, bs);
-  BLOCK(all.v, 0, 0, 1, bs).setConstant(1);
-  BLOCK(all.v, 1, 0, ni, bs) = inp.v;
+  all.v.block( 0, 0, 1, bs).setConstant(1);
+  all.v.block( 1, 0, ni, bs) = inp.v;
   if (last < 0)
-    BLOCK(all.v, 1 + ni, 0, no, bs).setConstant(0);
+    all.v.block( 1 + ni, 0, no, bs).setConstant(0);
   else
-    BLOCK(all.v, 1 + ni, 0, no, bs) = out[last].v;
+    all.v.block( 1 + ni, 0, no, bs) = out[last].v;
 }
 void backward_stack1(Batch &all, Batch &inp, Sequence &out, int last) {
   assert(inp.cols() == out.cols());
@@ -263,33 +362,33 @@ void backward_stack1(Batch &all, Batch &inp, Sequence &out, int last) {
   int ni = inp.rows();
   int no = out.rows();
   int nf = ni + no + 1;
-  inp.d += BLOCK(all.d, 1, 0, ni, bs);
-  if (last >= 0) out[last].d += BLOCK(all.d, 1 + ni, 0, no, bs);
+  inp.d += all.d.block( 1, 0, ni, bs);
+  if (last >= 0) out[last].d += all.d.block( 1 + ni, 0, no, bs);
 }
 
 // combine the delayed gated state with the gated input
 void forward_statemem(Batch &state, Batch &ci, Batch &gi, Sequence &states,
                       int last, Batch &gf) {
-  state.v = EMUL(ci.v, gi.v);
-  if (last >= 0) state.v += EMUL(gf.v, states[last].v);
+  state.v = ci.v.array() * gi.v.array();
+  if (last >= 0) state.v.array()+= gf.v.array() *  states[last].v.array();
 }
 void backward_statemem(Batch &state, Batch &ci, Batch &gi, Sequence &states,
                        int last, Batch &gf) {
-  if (last >= 0) states[last].d.A += state.d.A * gf.v.A;
-  if (last >= 0) gf.d.A += state.d.A * states[last].v.A;
-  gi.d.A += state.d.A * ci.v.A;
-  ci.d.A += state.d.A * gi.v.A;
+  if (last >= 0) states[last].d.array() += state.d.array() * gf.v.array();
+  if (last >= 0) gf.d.array() += state.d.array() * states[last].v.array();
+  gi.d.array() += state.d.array() * ci.v.array();
+  ci.d.array() += state.d.array() * gi.v.array();
 }
 
 // nonlinear gated output
 template <class H>
 void forward_nonlingate(Batch &out, Batch &state, Batch &go) {
-  out.v = EMUL(nonlin<H>(state.v), go.v);
+  out.v = nonlin<H>(state.v).array() * go.v.array();
 }
 template <class H>
 void backward_nonlingate(Batch &out, Batch &state, Batch &go) {
-  go.d.A += nonlin<H>(state.v).A * out.d.A;
-  state.d.A += xprime<H>(state.v).A * go.v.A * out.d.A;
+  go.d.array() += nonlin<H>(state.v).array() * out.d.array();
+  state.d.array() += xprime<H>(state.v).array() * go.v.array() * out.d.array();
 }
 
 template void forward_nonlingate<TanhNonlin>(Batch &out, Batch &state,
@@ -308,99 +407,4 @@ template void backward_nonlingate<NoNonlin>(Batch &out, Batch &state,
 template void backward_nonlingate<ReluNonlin>(Batch &out, Batch &state,
                                               Batch &go);
 
-void randgauss(Mat &m) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::normal_distribution<double> randn;
-  for (int i = 0; i < ROWS(m); i++)
-    for (int j = 0; j < COLS(m); j++) m(i, j) = randn(gen);
-}
-void randgauss(Vec &v) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::normal_distribution<double> randn;
-  for (int i = 0; i < ROWS(v); i++) v(i) = randn(gen);
-}
-void randinit(Mat &m, float s, const string mode) {
-  if (mode == "unif") {
-    m.setRandom();
-    m = (2 * s * m).array() - s;
-  } else if (mode == "pos") {
-    m.setRandom();
-    m = m * s;
-  } else if (mode == "normal") {
-    randgauss(m);
-    m = m * s;
-  }
-}
-void randinit(Vec &m, float s, const string mode) {
-  if (mode == "unif") {
-    m.setRandom();
-    m = (2 * s * m).array() - s;
-  } else if (mode == "pos") {
-    m.setRandom();
-    m = m * s;
-  } else if (mode == "normal") {
-    randgauss(m);
-    m = m * s;
-  }
-}
-void randinit(Batch &m, int no, int ni, float s, const string mode) {
-  m.resize(no, ni);
-  randinit(m.v, s, mode);
-}
-void randinit(Mat &m, int no, int ni, float s, const string mode) {
-  m.resize(no, ni);
-  randinit(m, s, mode);
-}
-void randinit(Vec &m, int no, float s, const string mode) {
-  m.resize(no);
-  randinit(m, s, mode);
-}
-void zeroinit(Mat &m, int no, int ni) {
-  m.resize(no, ni);
-  m.setZero();
-}
-void zeroinit(Vec &m, int no) {
-  m.resize(no);
-  m.setZero();
-}
-
-void resize(Sequence &seq, int nsteps, int dims, int bs) {
-  seq.resize(nsteps);
-  for (int i = 0; i < nsteps; i++) seq[i].resize(dims, bs);
-}
-int size(Sequence &seq, int dim) {
-  if (dim == 0) return seq.size();
-  if (dim == 1) return seq[0].rows();
-  if (dim == 2) return seq[0].cols();
-  THROW("bad dim ins size");
-  return -1;
-}
-
-typedef vector<int> Classes;
-typedef vector<Classes> BatchClasses;
-
-Vec timeslice(const Sequence &s, int i, int b) {
-  Vec result(s.size());
-  for (int t = 0; t < s.size(); t++) result[t] = s[t].v(i, b);
-  return result;
-}
-
-bool anynan(Mat &a) {
-  for (int j = 0; j < ROWS(a); j++) {
-    for (int k = 0; k < COLS(a); k++) {
-      float x = a(j, k);
-      if (isnan(x)) return true;
-    }
-  }
-}
-bool anynan(Batch &a) {
-  return anynan(a.v) || anynan(a.d);
-}
-bool anynan(Sequence &a) {
-  for (int i = 0; i < a.size(); i++)
-    if (anynan(a[i])) return true;
-  return false;
-}
 }
