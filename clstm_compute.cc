ecc045749c9c1a88669f6ca2bb6a930849e7d6c8
Tom <tmb@ixion>
2015-11-08 11:42:05 -0800

    nonlinearities are now separate from linear layers

--- clstm_compute.cc
+++ clstm_compute.cc
@@ -56,9 +56,102 @@ Nonlinearity nonlinearities[] = {
   }
 };
 
+void forward_identity(Device *dev, Batch &y, Batch &x) {
+  y.v().device(*dev) = x.v();
+}
+void forward_sigmoid(Device *dev, Batch &y, Batch &x) {
+  y.v().device(*dev) = x.v().sigmoid();
+}
+void forward_tanh(Device *dev, Batch &y, Batch &x) {
+  y.v().device(*dev) = x.v().tanh();
+}
+void forward_relu(Device *dev, Batch &y, Batch &x) {
+  y.v().device(*dev) = x.v().unaryExpr([](Float x) { return x<=0?0:x; });
+}
+void forward_nonlin(Device *dev, Batch &y, Batch &x, int nl) {
+  switch(nl) {
+  case LIN: forward_identity(dev, y, x); break;
+  case SIG: forward_sigmoid(dev, y, x); break;
+  case TANH: forward_tanh(dev, y, x); break;
+  case RELU: forward_relu(dev, y, x); break;
+  default: abort();
+  }
+}
+
+void backward_identity(Device *dev, Batch &y) {
+  y.d().device(*dev) = y.d();
+}
+void backward_sigmoid(Device *dev, Batch &y) {
+  y.d().device(*dev) = y.v() * (-y.v()+Float(1)) * y.d();
+}
+void backward_tanh(Device *dev, Batch &y) {
+  y.d().device(*dev) = (-y.v()*y.v() + Float(1)) * y.d();
+}
+void backward_relu(Device *dev, Batch &y) {
+  y.d().device(*dev) = y.d() * 
+    y.v().unaryExpr([](Float y) { return Float(y<=0?0:1); });
+}
+void backward_nonlin(Device *dev, Batch &y, int nl) {
+  switch(nl) {
+  case LIN: backward_identity(dev, y); break;
+  case SIG: backward_sigmoid(dev, y); break;
+  case TANH: backward_tanh(dev, y); break;
+  case RELU: backward_relu(dev, y); break;
+  default: abort();
+  }
+}
+
+void backward_identity(Device *dev, Batch &y, Batch &x) {
+  x.d().device(*dev) += y.d();
+}
+void backward_sigmoid(Device *dev, Batch &y, Batch &x) {
+  x.d().device(*dev) += y.v() * (-y.v()+Float(1)) * y.d();
+}
+void backward_tanh(Device *dev, Batch &y, Batch &x) {
+  x.d().device(*dev) += (-y.v()*y.v() + Float(1)) * y.d();
+}
+void backward_relu(Device *dev, Batch &y, Batch &x) {
+  x.d().device(*dev) += y.d() * 
+    y.v().unaryExpr([](Float y) { return Float(y<=0?0:1); });
+}
+void backward_nonlin(Device *dev, Batch &y, Batch &x, int nl) {
+  switch(nl) {
+  case LIN: backward_identity(dev, y, x); break;
+  case SIG: backward_sigmoid(dev, y, x); break;
+  case TANH: backward_tanh(dev, y, x); break;
+  case RELU: backward_relu(dev, y, x); break;
+  default: abort();
+  }
+}
+
 // full layers with constant offset
 
+void forward_lin1(Device *dev, Batch &y, Params &W1, Batch &x) {
+  int n = W1.v.dimension(0), m = W1.v.dimension(1);
+  int bs = x.v.dimension(1);
+  assert(y.rows() == n);
+  assert(y.cols() == x.cols());
+  assert(x.rows() == m-1);
+  y.v().device(*dev) =
+      (W1.v().slice(indexes(0, 1), indexes(n, m - 1)).contract(x.v(), axispairs(1, 0)) +
+       W1.v().chip(0, 1).reshape(indexes(n, 1)).broadcast(indexes(1, bs)));
+}
+void backward_lin1(Device *dev, Batch &y, Params &W1, Batch &x) {
+  int n = W1.v.dimension(0), m = W1.v.dimension(1);
+  x.d().device(*dev) += W1.v().slice(indexes(0, 1), indexes(n, m - 1)).contract(y.d(), axispairs(0, 0));
+  W1.d().slice(indexes(0, 1), indexes(n, m - 1)).device(*dev) += y.d().contract(x.v(), axispairs(1, 1));
+  W1.d().chip(0, 1).device(*dev) += y.d().sum(indexes(1));
+}
+
+// full layers with nonlinearities
+
+#if 1
 void forward_full1(Device *dev, Batch &y, Params &W1, Batch &x, Nonlin nl) {
+  forward_lin1(dev, y, W1, x);
+  forward_nonlin(dev, y, y, nl);
+}
+#else
+void forward_full1(Device *dev,Batch &y, Params &W1, Batch &x, Nonlin nl) {
   Float (*f)(Float) = nonlinearities[nl].nonlin;
   int n = W1.v.dimension(0), m = W1.v.dimension(1);
   int bs = x.v.dimension(1);
@@ -69,6 +162,15 @@ void forward_full1(Device *dev, Batch &y, Params &W1, Batch &x, Nonlin nl) {
       (W1.v().slice(indexes(0, 1), indexes(n, m - 1)).contract(x.v(), axispairs(1, 0)) +
        W1.v().chip(0, 1).reshape(indexes(n, 1)).broadcast(indexes(1, bs))).unaryExpr(f);
 }
+#endif
+
+
+#if 1
+void backward_full1(Device *dev, Batch &y, Params &W1, Batch &x, Nonlin nl) {
+  backward_nonlin(dev, y, nl);
+  backward_lin1(dev, y, W1, x);
+}
+#else
 void backward_full1(Device *dev, Batch &y, Params &W1, Batch &x, Nonlin nl) {
   Float (*g)(Float) = nonlinearities[nl].yderiv;
   int n = W1.v.dimension(0), m = W1.v.dimension(1);
@@ -77,6 +179,7 @@ void backward_full1(Device *dev, Batch &y, Params &W1, Batch &x, Nonlin nl) {
   W1.d().slice(indexes(0, 1), indexes(n, m - 1)).device(*dev) += temp.contract(x.v(), axispairs(1, 1));
   W1.d().chip(0, 1).device(*dev) += temp.sum(indexes(1));
 }
+#endif
 
 // softmax
 
@@ -167,18 +270,48 @@ void backward_statemem(Device *dev, Batch &state, Batch &ci, Batch &gi, Sequence
   ci.d().device(*dev) += state.d() * gi.v();
 }
 
+// linear gated output
+
+void forward_gate(Device *dev, Batch &out, Batch &nlstate, Batch &go) {
+  out.v().device(*dev) = nlstate.v() * go.v();
+}
+void backward_gate(Device *dev, Batch &out, Batch &nlstate, Batch &go) {
+  go.d().device(*dev) += nlstate.v() * out.d();
+  nlstate.d().device(*dev) += go.v() * out.d();
+}
+
 // nonlinear gated output
 
+#if 1
+void forward_nonlingate(Device *dev, Batch &out, Batch &state, Batch &go, int nl) {
+  Batch temp;
+  temp.resize(out.rows(), out.cols());
+  forward_nonlin(dev, temp, state, nl);
+  forward_gate(dev, out, temp, go);
+}
+#else
 void forward_nonlingate(Device *dev, Batch &out, Batch &state, Batch &go, Nonlin nl) {
   Float (*f)(Float) = nonlinearities[nl].nonlin;
   out.v().device(*dev) = state.v().unaryExpr(f) * go.v();
 }
+#endif
+
+#if 1
+void backward_nonlingate(Device *dev, Batch &out, Batch &state, Batch &go, int nl) {
+  Batch temp;
+  temp.resize(out.rows(), out.cols());
+  forward_nonlin(dev, temp, state, nl);
+  backward_gate(dev, out, temp, go);
+  backward_nonlin(dev, temp, state, nl);
+}
+#else
 void backward_nonlingate(Device *dev, Batch &out, Batch &state, Batch &go, Nonlin nl) {
   Float (*f)(Float) = nonlinearities[nl].nonlin;
   Float (*g)(Float) = nonlinearities[nl].xderiv;
   go.d().device(*dev) += state.v().unaryExpr(f) * out.d();
   state.d().device(*dev) += state.v().unaryExpr(g) * go.v() * out.d();
 }
+#endif
 
 }
 
