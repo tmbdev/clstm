2b9b85b65b1c7fb08989758bd32e1823a1911fd4
Konstantin Baierer <unixprog@gmail.com>
2017-12-16 19:06:40 +0100

    Drop unused 'glob' method from utils
    GLOB_TILDE is not implemented in musl

--- utils.h
+++ utils.h
@@ -44,16 +44,6 @@ inline double now() {
   return tv.tv_sec + 1e-6 * tv.tv_usec;
 }
 
-inline void glob(vector<string> &result, const string &arg) {
-  result.clear();
-  glob_t buf;
-  glob(arg.c_str(), GLOB_TILDE, nullptr, &buf);
-  for (int i = 0; i < buf.gl_pathc; i++) {
-    result.push_back(buf.gl_pathv[i]);
-  }
-  if (buf.gl_pathc > 0) globfree(&buf);
-}
-
 inline string basename(string s) {
   int start = 0;
   for (;;) {
